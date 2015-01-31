fb9088d9ead4abd323dabec025efd0b3e238aebc
Kent C. Dodds <kent+github@doddsfamily.us>
2015-01-30 22:10:33 -0700

    Improving the directive style

--- check-human.js
+++ check-human.js
@@ -1,22 +1,31 @@
-angular.module('app').directive('checkHuman', function checkHumanDirective() {
-  return {
-    restrict: 'E',
-    templateUrl: 'check-human.html',
-    scope: {
-      valid: '=isHuman'
-    },
-    controllerAs: 'vm',
-    bindToController: true,
-    controller: function checkHumanCtrl() {
-      var vm = this;
-      vm.valid = false;
-      vm.rand1 = Math.floor(Math.random() * 5) + 1;
-      vm.rand2 = Math.floor(Math.random() * 5) + 1;
-      var correct = vm.rand1 + vm.rand2;
+(function() {
+  'use strict';
 
-      vm.check = function() {
-        vm.valid = parseInt(vm.answer, 10) === correct;
+  angular.module('app').directive('checkHuman', checkHuman);
+
+  function checkHuman() {
+    return {
+      restrict: 'E',
+      templateUrl: 'check-human.html',
+      scope: {
+        valid: '=isHuman'
+      },
+      controllerAs: 'vm',
+      bindToController: true,
+      controller: function checkHumanCtrl() {
+        var vm = this;
+
+        vm.valid = false;
+        vm.rand1 = Math.floor(Math.random() * 5) + 1;
+        vm.rand2 = Math.floor(Math.random() * 5) + 1;
+        var correct = vm.rand1 + vm.rand2;
+
+        vm.check = check;
+
+        function check() {
+          vm.valid = parseInt(vm.answer, 10) === correct;
+        }
       }
-    }
-  };
-});
+    };
+  }
+})();
